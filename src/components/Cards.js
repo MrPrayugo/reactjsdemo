780bd73a1431603ddc6558158b294f3251b2855d
Angger Prayugo <anggerprayugo251@gmail.com>
2022-10-12 11:22:03 +0700

    finishing reactsjsdemo

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,35 +1,3 @@
-// import React from 'react'
-// import CardItem from './CardItem';
-// import './Cards.css';
-
-// function Cards() {
-//   return (
-//     <div className='cards'>
-//       <h1>Chec out these EPIC Destinations!</h1>
-//       <div className="cards__container">
-//         <div className="cards__wrapper">
-//           <ul className="cards__items">
-//             <CardItem
-//             src="images/img-9.jpg"
-//             text="Explore the hidden waterfall deep inside the Amazon Jungle"
-//             label='Adventure'
-//             path='/services'
-//             />
-//             <CardItem
-//             src="images/img-8.jpg"
-//             text="Explore the hidden waterfall deep inside the Amazon Jungle"
-//             label='Adventure'
-//             path='/services'
-//             />
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Cards
-
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
